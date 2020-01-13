dc0a87bbc32690a5408b6c78dc5107b2297b91ce
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-13 11:10:01 +0100

    ✨ MacOS | Key press and hold activated

--- macos/setup.sh
+++ macos/setup.sh
@@ -128,8 +128,8 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Follow the keyboard focus while zoomed in
 # defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# Enable press-and-hold for keys (useful for accents)
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool true
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
