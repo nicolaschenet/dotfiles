0811dd3bc811bcab8732be677c26c02dc9f28e16
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-13 15:06:31 +0100

    ➕ Brew | Added Aerial screensaver

--- install-dotfiles/src/tasks/homebrew/constants.ts
+++ install-dotfiles/src/tasks/homebrew/constants.ts
@@ -3,6 +3,7 @@ export const BREW_FORMULAE = ['git', 'gmp', 'htop', 'node', 'p7zip', 'yarn']
 export const BREW_CASKS = [
   '1clipboard',
   '1password',
+  'aerial',
   'docker',
   'github',
   'google-chrome',
