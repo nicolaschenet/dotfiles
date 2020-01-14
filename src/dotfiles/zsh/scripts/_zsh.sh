6a94ba6af63b916d1ac42ad417140426f310637d
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-15 00:06:14 +0100

    ♻️ ZSH | Better install

--- src/dotfiles/zsh/scripts/_zsh.sh
+++ src/dotfiles/zsh/scripts/_zsh.sh
@@ -2,7 +2,10 @@ export ZSH="$HOME/.oh-my-zsh"
 
 ZSH_THEME=powerlevel10k/powerlevel10k
 
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting)
+plugins=(git)
+
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 source $ZSH/oh-my-zsh.sh
 
