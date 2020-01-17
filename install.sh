6a38b525b46a5a764d535b5531148d1fa87683b8
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-17 17:13:12 +0100

    🚧 Install | Removed yarn from pre-install

--- install.sh
+++ install.sh
@@ -3,15 +3,8 @@
 # Install node
 sudo curl -Ls https://install-node.now.sh | sh;
 
-# Install yarn
-sudo curl -o- -L https://yarnpkg.com/install.sh | sh;
-
-# Make sure yarn executable is available
-[ -f ~/.bashrc ] && source ~/.bashrc
-[ -f ~/.zshrc ] && source ~/.zshrc
-
 # Install dependencies
-yarn;
+npm install;
 
 # Let's go
 ./bin/run;
