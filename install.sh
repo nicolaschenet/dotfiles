319a88d07aac0060e413df17a0eef2133a167d4a
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-23 22:40:19 +0100

    🔒 Install | Less sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env zsh
 
-sudo -v
-
-# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
 # Install node
 sudo curl -Ls https://install-node.now.sh | sh;
 
