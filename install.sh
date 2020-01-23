f3d8b18822bbdc801bc266644dedaa7ba75efb5a
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-23 21:20:14 +0100

    🔒 Install | Fix bad user

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env zsh
 
 # Install node
-curl -Ls https://install-node.now.sh | sh;
+sudo curl -Ls https://install-node.now.sh | sh;
+
+# Revert `.config` owner to current user
+sudo chown -R $(whoami) ~/.config
 
 # Install dependencies
 npm install;
