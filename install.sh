134d3477971f11977e33c4510aa83ba32af3ae52
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-23 22:35:01 +0100

    🔒 Install | Added sudo keep-alive

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env zsh
 
+sudo -v
+
+# Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install node
-sudo curl -Ls https://install-node.now.sh | sh;
+curl -Ls https://install-node.now.sh | sh;
 
 # Revert `.config` owner to current user
-sudo chown -R $(whoami) ~/.config
+chown -R $(whoami) ~/.config
 
 # Install dependencies
 npm install;
