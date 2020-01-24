fbbf04f28c4d003a244f8ddad1073f1ae477c186
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-24 12:00:31 -0500

    ⚡️ Dotfiles | Better vscode config path

--- src/tasks/dotfiles/index.ts
+++ src/tasks/dotfiles/index.ts
@@ -24,7 +24,7 @@ export const backupOldDotfiles = () => new Listr(Object.values(DOTFILES).map(fil
 })), { renderer: VerboseRenderer })
 
 export const prepareDotFilesDirs = () =>
-  execCommand(`mkdir -p ${HOME}/Library/Application Support/Code/User`)
+  shell.mkdir('-p', `${HOME}/Library/Application Support/Code/User`)
 
 export const installDotFiles = () => new Listr(Object.keys(DOTFILES).map(source => ({
   title: source,
