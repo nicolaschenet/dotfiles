7728fe607257f3aa09486a4a28c08b483f6741e6
Nicolas Chenet <nicolas.chenet@datadoghq.com>
2020-01-24 10:09:40 -0500

    🏗 Install | nvm

--- install.sh
+++ install.sh
@@ -1,14 +1,20 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-# Install node
-curl -Ls https://install-node.now.sh | sh;
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
 
-# Install dependencies
-npm install;
+# Activate nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-echo $(whoami);
+# Install node via nvm
+nvm install node
 
-# Revert `.config` owner to current user
+# Install project dependencies
+npm install
+
+# Revert `.config` owner to current user (better safe than sorry)
 chown -R $(whoami) ~/.config
 
 # Let's go
